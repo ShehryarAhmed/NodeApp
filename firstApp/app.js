259601a25a674c7bd4010b3a84ba67c34c9b4f29
shehreyarahmed <shehreyarahmed5@gmail.com>
2019-12-29 23:07:48 +0500

    working

---
+++ firstApp/app.js
@@ -0,0 +1,15 @@
+function hello(name ){
+    console.log('Hello '+name)
+}
+
+setTimeout(() => {
+    hello('Shehryar')
+    
+}, 3000);
+console.log()
+
+var message = 'food'
+console.log(module)
+
+var logger = require('./logger')
+logger.log('Bismillah')
---
+++ firstApp/logger.js
@@ -0,0 +1,7 @@
+var url = 'http://mylogger.io/log';
+
+function log(msg) {
+    console.log(msg)
+}
+
+module.exports.log = log;   
