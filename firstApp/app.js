84e3db64bc6d4ab0d3ee374a8a356b22379dbfc6
shehryarahmed <shehreyarahmed5@gmail.com>
2019-12-30 10:59:26 +0500

    now working on events module

--- firstApp/app.js
+++ firstApp/app.js
@@ -13,3 +13,11 @@ console.log(module)
 
 var logger = require('./logger')
 logger.log('Bismillah')
+
+
+//read directory
+const fs = require('fs');
+fs.readdir('$',function(err, files){
+    if(err) console.log('Error',err);
+    else console.log('Result',files)
+});
