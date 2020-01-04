98bf9709f3a27c056f80ff5f56f9027cd8fec480
shehreyarahmed <shehreyarahmed5@gmail.com>
2020-01-05 01:06:54 +0500

    working on post request

--- Express/index.js
+++ Express/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+var courses = [
+    {id:1,name:'Quran'},
+    {id:2,name:'Islam'},
+    {id:3,name:'Math'}
+]
 
 app.get('/',(req, res) => {
     res.send("AssalamOAlikum")
@@ -10,12 +15,13 @@ app.get('/api/salam',(req, res) => {
     res.send([1, 2, 3,4,]);
 });
 
-app.get('/api/salam/:id/:name', (req, res) => {
+app.get('/api/salam/:id', (req, res) => {
     // res.send(req.params);
-    res.send(`Hello ${req.params.name} query by ${req.query.sortBy}`);
-
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send('The course with given ID was not found')
+    // res.send(`Hello ${req.params.name} query by ${req.query.sortBy}`);
     // res.send(""+req.params+ ""+req.query);
-
+    res.send(course)
 });
 
 const port = process.env.PORT || 5000;
