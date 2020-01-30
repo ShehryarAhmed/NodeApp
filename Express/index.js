96379b7fb09953651e97172c131ac9af7d746b55
shehryarahmed <shehreyarahmed5@gmail.com>
2020-01-30 13:08:18 +0500

    complete chapter 5

--- Express/index.js
+++ Express/index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const Joi = require('joi');
 const courses = require('./routes/courses');
+const home = require('./routes/home');
+const home = require('./routes/home');
+const log = require('./middleware/logger');
+
 
 const app = express();
 
 app.set('view engine', 'pug');
-app.set('views', './views'); //default
+app.set('views', './views'); 
 
 app.use(express.json());
+app.use('/',home);
 app.use('/api/courses',courses);
 
 const port = process.env.PORT || 5000;
